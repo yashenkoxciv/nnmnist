33b98dd9f6b1cdebdb9f24db975e6d11b7c828b4
yashenkoxciv <42155519+yashenkoxciv@users.noreply.github.com>
2018-08-06 23:54:34 +0300

    Update __init__.py

--- mnistdata/__init__.py
+++ mnistdata/__init__.py
@@ -1,6 +1,2 @@
-#!/usr/bin/env python3
-''' To install locally (linking) use:
-    ln -s mnistdata ~/<conda>/lib/<python>/site-packages/
-    ln -s mnistdata ~/<conda>/envs/<env>/lib/<python>/site-packages/
-'''
+
 
